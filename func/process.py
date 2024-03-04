4cb90e82320a9bc7ed0c2e6e5b0f61ebbeba3884
czw <czw1296924847@stu.xjtu.edu.cn>
2024-03-04 23:08:59 +0800

    Add files via upload

--- func/process.py
+++ func/process.py
@@ -1,14 +1,103 @@
+import re
 import pandas as pd
+import h5py
 import torch
 import numpy as np
 import os
 import os.path as osp
+import inspect
 from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
 
-
 ROOT = "/home/chenziwei2021/standford_dataset"
 RE_AD = "/home/chenziwei2021/pyn/paper/EQGraphNet/web/estimate/static/result"
+DATA_AD = "/home/chenziwei2021/pyn/paper/EQGraphNet/web/estimate/static/data"
+WORD_AD = "/home/chenziwei2021/pyn/paper/EQGraphNet/web"
+PY_AD = "/home/chenziwei2021/anaconda3/envs/cmh/bin/python3.11"
+DEFAULT_MODELS = ["MagInfoNet", "EQGraphNet", "MagNet", "CREIME", "ConvNetQuakeINGV"]
+DEFAULT_LIBS = "import numpy as np\n"
+
+
+def is_error(string, no_warn=True):
+    """
+    Check whether the output contents contain error messages
+
+    :param string:
+    :param no_warn: Ignore Warning from STEAD
+    :return:
+    """
+    lines = string.split('\n')
+    if no_warn:
+        pattern = re.compile(r'.*DtypeWarning.*|.*self\.df = pd\.read_csv.*', re.MULTILINE)
+        lines = [line for line in lines if not pattern.search(line) and line != '']
+    errors = ['error', 'Error']
+    return any(error in str(line) for error in errors for line in lines)
+
+
+def get_source(obj, no_def=True):
+    """
+    Transform code to string
+
+    :param obj: Can be a module, class, method, function, traceback, frame, or code object
+    :param no_def: Remove definition and corresponding indents
+    :return:
+    """
+    source = inspect.getsource(obj)
+    if no_def:
+        # replace the definition statement with the empty string by re
+        no_def_source = re.sub(r'^\s*def\s+code_\w+\s*\(.*\):(?!.*\n\s*def\s+code_\w+\s*\(.*\):)', '', source, flags=re.MULTILINE)
+
+        # find and remove the minimum indentation for non-empty lines
+        lines = no_def_source.split('\n')
+        min_indent = min(len(line) - len(line.lstrip()) for line in lines if line.lstrip())
+        lines = lines[1:-1]
+        source = '\n'.join(line[min_indent:] for line in lines)
+    return source
+
+
+def get_lib_by_files(files, remain=True):
+    """
+    Get dependent libraries like "import numpy as np\nimport torch", from files
+
+    :param files: List of file names, like ["network.py", "process.py"]
+    :param remain: Remain (True) or remove (False) based on regular expression
+    :return:
+    """
+    codes = []
+    for file in files:
+        with open(file, 'r') as f:
+            contents = f.read()
+            code = get_library_by_string(contents, remain)
+            codes += code
+    library = "\n".join(codes)
+
+    # add default library in the beginning (must be added! Or you can try removing what might happen)
+    library = DEFAULT_LIBS + library
+    return library
+
+
+def get_library_by_string(string, remain):
+    """
+    Get dependent libraries , from string
+
+    :param string: like "import numpy as np\nimport torch"
+    :param remain:
+    :return: like ["import numpy as np", "import torch"]
+    """
+    import_pattern = re.compile(
+        r"^(from\s+[\w.]+\s+import\s+([\w.]+(\s*,\s*[\w.]+)*)(\s+as\s+\w+)?)|(^import\s+([\w.]+(\s*,\s*[\w.]+)*)(\s+as\s+\w+)?)|^(sys\.path\.append\(\"..\"\))",
+        re.MULTILINE)
+    if remain:
+        contents = import_pattern.findall(string)
+        code = [max(content, key=len) for content in contents]
+    else:
+        code = [content for content in string.splitlines() if not import_pattern.match(content)]
+    return code
+
+
+def duplicate_lib(string):
+    code = [content for content in string.splitlines()]
+    return "\n".join(list(dict.fromkeys(code)))
 
 
 def get_train_or_test_idx(num, num_train):
@@ -191,3 +280,83 @@ def get_dist(feature, bins, chunk_name, data_size, v_min=None, v_max=None):
     if v_max is not None:
         y[-1] = y[-1] - 1
     return x, y
+
+
+def be_numpy(x):
+    if torch.is_tensor(x):
+        return x.numpy()
+    elif isinstance(x, np.ndarray):
+        return x
+    else:
+        raise TypeError("Unknown type of x, must be 'tensor' or 'ndarray'!")
+
+
+class Chunk(Dataset):
+    def __init__(self, data_size, train, data_size_train, idx, root, chunk_name):
+        super(Chunk, self).__init__()
+        self.data_size, self.root, self.name = data_size, root, chunk_name
+        self.save_ad = osp.join(root, str(data_size))
+        self.df = pd.read_csv(osp.join(self.root, self.name + ".csv"))
+        self.data, self.index = self.get_sample()
+        self.df = self.df.iloc[self.index, :]
+        self.data_size_train = data_size_train
+        self.length = self.data.shape[2]
+        self.train = train
+        self.idx = idx
+        self.get_train_or_test()
+
+    def get_train_or_test(self):
+        self.data = self.data[self.idx, :, :]
+        self.index = self.index[self.idx]
+        self.df = self.df.iloc[self.idx, :]
+        return None
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        data, index = self.data[idx, :, :], self.index[idx]
+        return data, index
+
+    def get_sample(self):
+        if not osp.exists(self.save_ad):
+            os.makedirs(self.save_ad)
+        data_ad = osp.join(self.save_ad, "data.pt")
+        index_ad = osp.join(self.save_ad, "index.pt")
+        if osp.exists(data_ad) & osp.exists(index_ad):
+            data = torch.load(data_ad)
+            index = be_numpy(torch.load(index_ad))
+        else:
+            metadata = h5py.File(osp.join(self.root, self.name + ".hdf5"), 'r')
+
+            trace_name = self.df.loc[:, "trace_name"].values.reshape(-1)
+            index = np.random.choice(trace_name.shape[0], self.data_size, replace=False).tolist()
+
+            ev_list = self.df['trace_name'].to_list()
+            data = np.zeros(shape=(self.data_size, 3, 6000))
+            for c, i in enumerate(index):
+                ev_one = ev_list[i]
+                dataset_one = metadata.get('data/' + str(ev_one))
+                data_one = np.array(dataset_one)
+                data_one = np.expand_dims(data_one.T, axis=0)
+                data[c, :, :] = data_one
+
+            data = torch.from_numpy(data).float()
+            index = torch.FloatTensor(index).int()
+
+            torch.save(data, data_ad)
+            torch.save(index, index_ad)
+        return data, be_numpy(index)
+
+
+def get_mai_data(df_train, df_test):
+    ps_at_name = ["p_arrival_sample", "s_arrival_sample"]
+    ps_at_train, ps_at_test = df_train.loc[:, ps_at_name].values, df_test.loc[:, ps_at_name].values
+    prep_ps_at, ps_at_train, ps_at_test = prep_pt("sta", ps_at_train, ps_at_test)
+    ps_at_train, ps_at_test = torch.from_numpy(ps_at_train).float(), torch.from_numpy(ps_at_test).float()
+
+    t_name = ["p_travel_sec"]
+    p_t_train, p_t_test = df_train.loc[:, t_name].values, df_test.loc[:, t_name].values
+    prep_p_t, p_t_train, p_t_test = prep_pt("sta", p_t_train, p_t_test)
+    p_t_train, p_t_test = torch.from_numpy(p_t_train).float(), torch.from_numpy(p_t_test).float()
+    return ps_at_train, ps_at_test, p_t_train, p_t_test
