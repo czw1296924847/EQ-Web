0b2c3c4f970c391f656393c47eba1dc665a234bd
czw <czw1296924847@stu.xjtu.edu.cn>
2024-03-04 23:08:30 +0800

    Add files via upload

--- web/wsgi.py
+++ web/wsgi.py
@@ -11,10 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-import inspect
+from inspect import getsource
 import estimate.network as network
+import estimate.views as views
 import func.net as net
-from estimate.registry import MagRegistry
+from func.process import ROOT, DATA_AD
+from func.process import get_lib_by_files, get_source
+from estimate.registry import DlRegistry
+from estimate.static.detail import MagInfoNet, EQGraphNet, MagNet, CREIME, ConvNetQuakeINGV, TestNet
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
 
@@ -23,57 +27,82 @@ application = get_wsgi_application()
 """
 registry user and model, for operation estimation
 """
-registry = MagRegistry()
+registry = DlRegistry()
 
 registry.add_model(model_object=network.MagInfoNet(),
-                   model_name="MagInfoNet",
-                   model_status="Production",
-                   model_version="0.0.1",
-                   model_owner="Chen Ziwei",
-                   model_situation="Free",
-                   model_description="Proposed model",
-                   model_code=inspect.getsource(net.MagInfoNet))
+                   name="MagInfoNet",
+                   description="Proposed model",
+                   owner="Chen Ziwei",
+                   path_data=ROOT,
+                   library=get_source(MagInfoNet.code_lib),
+                   code_data=get_source(MagInfoNet.code_data),
+                   code_model=get_source(MagInfoNet.MagInfoNet, False),
+                   code_train=get_source(MagInfoNet.code_train),
+                   code_test=get_source(MagInfoNet.code_test),
+                   code_run=get_source(MagInfoNet.code_run),)
 
 registry.add_model(model_object=network.EQGraphNet(),
-                   model_name="EQGraphNet",
-                   model_status="Production",
-                   model_version="0.0.1",
-                   model_owner="Chen Ziwei",
-                   model_situation="Free",
-                   model_description="Proposed model",
-                   model_code=inspect.getsource(net.EQGraphNet))
+                   name="EQGraphNet",
+                   description="Proposed model",
+                   owner="Chen Ziwei",
+                   path_data=ROOT,
+                   library=get_source(EQGraphNet.code_lib),
+                   code_data=get_source(EQGraphNet.code_data),
+                   code_model=get_source(EQGraphNet.EQGraphNet, False),
+                   code_train=get_source(EQGraphNet.code_train),
+                   code_test=get_source(EQGraphNet.code_test),
+                   code_run=get_source(EQGraphNet.code_run),)
 
 registry.add_model(model_object=network.MagNet(),
-                   model_name="MagNet",
-                   model_status="Production",
-                   model_version="0.0.1",
-                   model_owner="Mousavi",
-                   model_situation="Free",
-                   model_description="From doi.org/10.1029/2019GL085976",
-                   model_code=inspect.getsource(net.MagNet))
+                   name="MagNet",
+                   description="From 10.1029/2019GL085976",
+                   owner="Mousavi",
+                   path_data=ROOT,
+                   library=get_source(MagNet.code_lib),
+                   code_data=get_source(MagNet.code_data),
+                   code_model=get_source(MagNet.MagNet, False),
+                   code_train=get_source(MagNet.code_train),
+                   code_test=get_source(MagNet.code_test),
+                   code_run=get_source(MagNet.code_run),)
 
 registry.add_model(model_object=network.CREIME(),
-                   model_name="CREIME",
-                   model_status="Production",
-                   model_version="0.0.1",
-                   model_owner="Chakraborty",
-                   model_situation="Free",
-                   model_description="From doi.org/10.1029/2022JB024595",
-                   model_code=inspect.getsource(net.CREIME))
+                   name="CREIME",
+                   description="From 10.1029/2022JB024595",
+                   owner="Chakraborty",
+                   path_data=ROOT,
+                   library=get_source(CREIME.code_lib),
+                   code_data=get_source(CREIME.code_data),
+                   code_model=get_source(CREIME.CREIME, False),
+                   code_train=get_source(CREIME.code_train),
+                   code_test=get_source(CREIME.code_test),
+                   code_run=get_source(CREIME.code_run),)
 
 registry.add_model(model_object=network.ConvNetQuakeINGV(),
-                   model_name="ConvNetQuakeINGV",
-                   model_status="Production",
-                   model_version="0.0.1",
-                   model_owner="Lomax",
-                   model_situation="Free",
-                   model_description="From article-abstract/90/2A/517/568771",
-                   model_code=inspect.getsource(net.ConvNetQuakeINGV))
+                   name="ConvNetQuakeINGV",
+                   description="From 90/2A/517/568771",
+                   owner="Lomax",
+                   path_data=ROOT,
+                   library=get_source(ConvNetQuakeINGV.code_lib),
+                   code_data=get_source(ConvNetQuakeINGV.code_data),
+                   code_model=get_source(ConvNetQuakeINGV.ConvNetQuakeINGV, False),
+                   code_train=get_source(ConvNetQuakeINGV.code_train),
+                   code_test=get_source(ConvNetQuakeINGV.code_test),
+                   code_run=get_source(ConvNetQuakeINGV.code_run),)
 
+registry.add_model(model_object=None,
+                   name="TestNet",
+                   description="自定义模型示例，上传代码、运行",
+                   owner="Chen Ziwei",
+                   path_data=DATA_AD,
+                   library=get_source(TestNet.code_lib),
+                   code_data=get_source(TestNet.code_data),
+                   code_model=get_source(TestNet.TestNet, False),
+                   code_train=get_source(TestNet.code_train),
+                   code_test=get_source(TestNet.code_test),
+                   code_run=get_source(TestNet.code_run))
 
 registry.add_user(username="czw",
                   password="fff")
-
 
 registry.add_feature(param="source_magnitude",
                      description="Released seismic energy of earthquake")
@@ -92,6 +121,12 @@ registry.add_feature(param="p_arrival_sample",
 
 registry.add_feature(param="s_arrival_sample",
                      description="S wave arrival time")
+
+registry.add_feature(param="source_latitude",
+                     description="Latitude of earthquake source")
+
+registry.add_feature(param="source_longitude",
+                     description="Longitude of earthquake source")
 
 registry.init_info()
 
